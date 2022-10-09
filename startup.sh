3b3b5d84777f41e5d914bc2a4bddc65a30316e73
Jason <jason@ChaoFamily.localdomain>
2022-10-08 23:54:23 -0400

    Updated docker run script to map docker ports to localhost ports.

--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [[ $(which docker) ]]; then
-  docker pull team0anana/csc302:frontend
   docker pull team0anana/csc302:backend
+  docker pull team0anana/csc302:frontend
 
-  docker run -d team0anana/csc302:backend
-  docker run team0anana/csc302:frontend
+  docker run -d -p 5000:5000 team0anana/csc302:backend
+  docker run -d -p 3000:3000 team0anana/csc302:frontend
 else
   echo "You need to install docker"
 fi
