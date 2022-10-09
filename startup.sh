872d0f362f74c6909fc4ce33b7036de0a0a4d0a7
Camran Hansen <camran.hansen@mail.utoronto.ca>
2022-10-09 19:50:48 -0400

    Add more descriptive startup.sh

--- startup.sh
+++ startup.sh
@@ -7,6 +7,9 @@ if [[ $(which docker) ]]; then
   docker run -d -p 5000:5000 --name team0anana-backend team0anana/csc302:backend
   docker run -d -p 3000:3000 --name team0anana-frontend team0anana/csc302:frontend
 
+  echo "React template running at http://localhost:3000/"
+  echo "Flask backend running at http://localhost:5000/hello"
+  echo "Database running at http://localhost:4000/"
 else
   echo "You need to install docker"
 fi
