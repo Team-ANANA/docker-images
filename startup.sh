9e5b99995fcb735bbf3a22238239593d933d7d07
osmanafandi <osman.afandiyev@mail.utoronto.ca>
2022-10-09 13:39:30 -0400

    naming each running container

--- startup.sh
+++ startup.sh
@@ -4,8 +4,8 @@ if [[ $(which docker) ]]; then
   docker pull team0anana/csc302:frontend
   docker pull team0anana/csc302:backend
 
-  docker run -d team0anana/csc302:backend
-  docker run team0anana/csc302:frontend
+  docker run -d --name team0anana-backend team0anana/csc302:backend
+  docker run -d --name team0anana-frontend team0anana/csc302:frontend
 else
   echo "You need to install docker"
 fi
