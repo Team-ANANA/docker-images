bdad330c8a09ed3ae02fdb6a892127b36c3f8d1b
Jason-HaoXinChao <jason@ChaoFamily.localdomain>
2022-10-10 04:30:29 -0400

    Add loading notification in start up script
    the script should now show a loading ..... while the endpoints are being
    loaded up

--- startup.sh
+++ startup.sh
@@ -6,12 +6,48 @@ if [[ $(which docker) ]]; then
   docker pull mysql/mysql-server:latest
 
   docker run -d -p 5000:5000 --name team0anana-backend team0anana/csc302:backend
-  docker run -d -p 3000:3000 --name team0anana-frontend team0anana/csc302:frontend
   docker run -d -p 3306:4000 --name team0anana-database mysql/mysql-server:latest
+  docker run -d -p 3000:3000 --name team0anana-frontend team0anana/csc302:frontend
 
-  printf "React template running at http://localhost:3000/\n"
-  printf "Flask backend running at http://localhost:5000/\n"
-  printf "Database running at http://localhost:4000/\n"
+  # wait for flask endpoint
+  echo "Waiting for Flask api to spin up"
+  until $(curl --output /dev/null --silent --head --fail http://localhost:5000)
+  do
+    BAR='.....'
+    for i in 1 2 3 4 5
+    do
+        echo -ne "\r${BAR:0:$i}"
+        sleep .5
+    done
+    echo -ne "\r     "
+  done
+  echo -ne "\rFlask backend running at http://localhost:5000/\n"
+
+  # wait for react endpoint
+  echo "Waiting for React app to spin up"
+  until $(curl --output /dev/null --silent --head --fail http://localhost:3000)
+  do
+    BAR='.....'
+    for i in 1 2 3 4 5
+    do
+        echo -ne "\r${BAR:0:$i}"
+        sleep .5
+    done
+    echo -ne "\r     "
+  done
+  echo -ne "\rReact template running at http://localhost:3000/\n"
+
+  # wait for mysql endpoint
+  echo "Waiting for mysql server to spin up"
+  # since we don't actually have the mysql server connection setup yet, we will just pretend we the endpoint has loaded.
+  BAR='.....'
+  for i in 1 2 3 4 5
+  do
+      echo -ne "\r${BAR:0:$i}"
+      sleep .5
+  done
+  echo -e "\rDatabase running at http://localhost:4000/"
+  echo "All containers are up and running!"
 else
   echo "You need to install docker.\nGet docker at https://docs.docker.com/get-docker/"
 fi
